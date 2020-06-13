968e06450ac2f376dee93334ce72e442ff5bd2e1
Noer Paanakker <noer@lightworksweb.nl>
2020-06-13 21:11:52 +0200

    in signup added doc creation linked with auth user

--- functions/controllers/auth-controller.js
+++ functions/controllers/auth-controller.js
@@ -4,14 +4,8 @@ const admin = require('firebase-admin');
 const signup = async (req, res) => {
 	const { email, password, confirmPassword, handle } = req.body;
 
-	const newUser = {
-		email,
-		password,
-		handle,
-	};
-
 	try {
-		const foundUser = await admin.firestore().doc(`/users/${newUser.handle}`).get();
+		const foundUser = await admin.firestore().doc(`/users/${handle}`).get();
 
 		if (foundUser.exists) {
 			return res.status(400).json({ message: 'This handle is already taken.' });
@@ -21,9 +15,21 @@ const signup = async (req, res) => {
 			return res.status(422).json({ message: "Passwords don't match!" });
 		}
 
-		const createdUser = await firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+		// Create user for auth
+		const createdUser = await firebase.auth().createUserWithEmailAndPassword(email, password);
 
+		// Create auth token
 		const idToken = await createdUser.user.getIdToken();
+
+		const userCreds = {
+			email,
+			handle,
+			createdAt: new Date().toISOString(),
+			userId: createdUser.user.uid,
+		};
+
+		// Create user doc in collection
+		await admin.firestore().doc(`/users/${userCreds.handle}`).set(userCreds);
 
 		return res.status(201).json({ message: `A new user with token ${idToken} has been created!` });
 	} catch (err) {
