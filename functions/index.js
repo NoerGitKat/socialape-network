70e29b8abe00c62d4179dbd774774c48dbce7ac4
Noer Paanakker <noer@lightworksweb.nl>
2020-06-13 09:33:24 +0200

    added new createScream function

--- functions/index.js
+++ functions/index.js
@@ -4,17 +4,39 @@ const admin = require('firebase-admin');
 // Init application
 admin.initializeApp();
 
-exports.getScreams = functions.https.onRequest(async (request, response) => {
+exports.getScreams = functions.https.onRequest(async (req, res) => {
 	try {
 		// Access Firebase DB
 		const screamsCollection = await admin.firestore().collection('screams').get();
-		let screams = [];
+		const screams = [];
+		// Get and push objects in variable
 		const screamDocs = await screamsCollection.forEach((doc) => {
 			screams.push(doc.data());
 		});
-		return response.json(screams);
+		return res.json(screams);
 	} catch (err) {
 		console.error(err);
+		return res.status(500).json(err);
 	}
-	response.send();
+});
+
+exports.createScream = functions.https.onRequest(async (req, res) => {
+	if (req.method !== 'POST') {
+		return res.status(400).json({ error: 'Method not allowed!' });
+	}
+
+	const { handle, body } = req.body;
+	const newScream = {
+		handle,
+		body,
+		createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+	};
+
+	try {
+		const newScreamDoc = await admin.firestore().collection('screams').add(newScream);
+		return res.json({ message: `Successfully created new document with ID ${newScreamDoc.id}!` });
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json(err);
+	}
 });
