69f283cf60578e95ca400cdcf1f3f09f043aa6df
Noer Paanakker <noer@lightworksweb.nl>
2020-06-19 17:02:27 +0200

    put api key in env variables

--- functions/index.js
+++ functions/index.js
@@ -10,16 +10,7 @@ const screamsRouter = require('./routes/screams-router');
 const authRouter = require('./routes/auth-router');
 
 const serviceAccount = require('./serviceAccountKey.json');
-const firebaseConfig = {
-	apiKey: 'AIzaSyCpDis559px2NoQO0WACl0S4xpo_RPihtI',
-	authDomain: 'socialape-8cb27.firebaseapp.com',
-	databaseURL: 'https://socialape-8cb27.firebaseio.com',
-	projectId: 'socialape-8cb27',
-	storageBucket: 'socialape-8cb27.appspot.com',
-	messagingSenderId: '594538726867',
-	appId: '1:594538726867:web:ee521546446026278b9b26',
-	measurementId: 'G-PVC3W5P5LD',
-};
+const firebaseConfig = require('./utils/getConfig')();
 
 // Init application
 admin.initializeApp({
---
+++ functions/utils/getConfig.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+let config = require('./../../env.json');
+
+if (Object.keys(functions.config()).length) {
+	config = functions.config();
+}
+
+const getConfig = () => {
+	return {
+		apiKey: config.socialape.firebase_api_key,
+		authDomain: 'socialape-8cb27.firebaseapp.com',
+		databaseURL: 'https://socialape-8cb27.firebaseio.com',
+		projectId: 'socialape-8cb27',
+		storageBucket: 'socialape-8cb27.appspot.com',
+		messagingSenderId: '594538726867',
+		appId: config.socialape.app_id,
+		measurementId: 'G-PVC3W5P5LD',
+	};
+};
+
+module.exports = getConfig;
