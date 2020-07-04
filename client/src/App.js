46d5de79b13a61572d7f1614f60b9672207cbb6e
Noer Paanakker <noer@lightworksweb.nl>
2020-07-04 10:47:17 +0200

    added mui, created basic theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import './App.css';
+// Components
+import Navbar from './components/Navbar';
+
+// Routes
+import HomePage from './pages/home';
+import SignupPage from './pages/signup';
+import LoginPage from './pages/login';
+
+// Styles
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './styles/theme';
+import './styles/App.scss';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+		<MuiThemeProvider theme={theme}>
+			<div className="App">
+				<Router>
+					<Navbar />
+					<div className="container">
+						<Switch>
+							<Route exact path="/" component={HomePage} />
+							<Route exact path="/signup" component={SignupPage} />
+							<Route exact path="/login" component={LoginPage} />
+						</Switch>
+					</div>
+				</Router>
+			</div>
+		</MuiThemeProvider>
 	);
 }
 
